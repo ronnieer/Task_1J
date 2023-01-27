97d3935c759cf3b0a2f542351eba2d97f9197ad6
ronnieer <ronnie.eriksson@yaelev.se>
2023-01-27 09:56:24 +0100

    kjsdkhgfd

--- main.c
+++ main.c
@@ -15,6 +15,6 @@ void main(void)
     printf("%s%s", "\n\tTask One:", "\n");
     printf("%s%s%s", "\n\tMy Name Is ", strMyName, "\n");
     printf("%s%d%s", "\n\tThe Name Contains ", lenMyName, " Characters\n\n\n");
-
+    printf("%s%d%s", "\n\tThe Name Contains ", lenMyName, " Characters\n\n\n");
     return;
 }
