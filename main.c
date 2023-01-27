ddcb9235631824dd3aa81e91026cb58afce337dd
ronnieer <ronnie.eriksson@yaelev.se>
2023-01-27 10:49:50 +0100

    My Commit

--- main.c
+++ main.c
@@ -15,7 +15,5 @@ void main(void)
     printf("%s%s", "\n\tTask One:", "\n");
     printf("%s%s%s", "\n\tMy Name Is ", strMyName, "\n");
     printf("%s%d%s", "\n\tThe Name Contains ", lenMyName, " Characters\n\n\n");
-    printf("%s%d%s", "\n\tThe Name Contains ", lenMyName, " Characters\n\n\n");
-    printf("%s%d%s", "\n\tThe Name Contains ", lenMyName, " Characters\n\n\n");
     return;
 }
